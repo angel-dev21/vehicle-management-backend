6064a6bd49962c9308aa84ca1fe8d3d78a0ec1c9
angel-dev21 <146152337+angel-dev21@users.noreply.github.com>
2026-06-18 02:04:36 -0500

    add CI workflow for tests and read DEBUG from env (django-environ)

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env()
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +32,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
