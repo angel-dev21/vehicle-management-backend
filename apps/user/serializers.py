8ede1d9c0d67f7f27eb1fa494dbc38ca206b4e50
angel-dev21 <146152337+angel-dev21@users.noreply.github.com>
2026-06-18 19:59:47 -0500

    restrict role field to dedicated admin-only endpoint

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import User
 
-class UserCreateSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     
     password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'password']
+        fields = ['id', 'email', 'first_name', 'last_name', 'password']
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import User
-from .serializers import UserCreateSerializer
+from .serializers import UserSerializer
 from backend.permissions import IsAdmin, IsSelfOrAdmin
 
 # Create your views here.
 
+class UserRoleUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['role']
+
 class UserViewSet(viewsets.ModelViewSet):
-    
     queryset = User.objects.filter(is_active=True)
-    serializer_class = UserCreateSerializer
+    serializer_class = UserSerializer
 
     def get_permissions(self):
-        if self.action in ['create', 'list', 'destroy']:
+        if self.action in ['create', 'list', 'destroy', 'set_role']:
             return [IsAdmin()]
         return [IsSelfOrAdmin()]
+
+    @action(detail=True, methods=['patch'], url_path='role')
+    def set_role(self, request, pk=None):
+        user = self.get_object()
+        serializer = UserRoleUpdateSerializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
         user = self.get_object()
